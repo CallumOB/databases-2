e3b1b50145400875a6c12e50773d78ec0106761d
CallumOB <callumobrien2004@gmail.com>
2023-09-21 18:14:53 +0100

    Lab 1 Finished

--- Week1/Script.sql
+++ Week1/Script.sql
@@ -45,4 +45,29 @@ where customer_name = 'Mary Martin');
 -- part h
 select staff_name from b2_staff 
 left join b2_corder on b2_corder.staffissued = b2_staff.staff_no
+where corderno in (
+	select corderno from b2_corder
+	left join b2_corderline using (corderno)
+	where stock_code in (
+		select stock_code from b2_corderline
+		left join b2_stock using (stock_code)
+		where supplier_id in (
+			select supplier_id from b2_stock 
+			right join b2_supplier using (supplier_id)
+			where supplier_name = 'Buckleys')));
+		
+-- part i 
+select supplier_id, sorderno from b2_sorder
+where delivereddate - sorderdate >= 5;
+
+-- part j
+select corderno, stock_code, (unit_price * quantityrequired) as cost from b2_corderline
+join b2_stock using (stock_code);
+
+-- part k
+select corderno, sum(unit_price * quantityrequired) as total_cost from b2_stock 
+join b2_corderline on b2_corderline.stock_code = b2_stock.stock_code
+group by corderno
+order by corderno asc;
+
 
