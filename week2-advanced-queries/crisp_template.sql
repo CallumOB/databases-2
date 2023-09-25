0c0d4a29131ad98083e066d34b7e4a5750b20cf6
CallumOB <callumobrien2004@gmail.com>
2023-09-25 17:00:43 +0100

    added extra file

---
+++ week2-advanced-queries/crisp_template.sql
@@ -0,0 +1,68 @@
+select user;
+/*select * from crisp_type;
+select * from consumer;
+select * from has_eaten;
+*/
+/*                    MAKE THE VIEWS                            */
+--Make a view showing the full join
+create or replace view crispeater as 
+  SELECT CN_NAME, CP_NAME FROM CONSUMER
+  FULL JOIN HAS_EATEN USING (CONSUMERID)
+  FULL JOIN CRISP_TYPE USING (CRISPKEY);
+--Contents of full join
+select * from crispeater;
+
+/*          ONLY        */
+
+--'Consumers who only ate ''Cheese and Onion''
+select CN_NAME from crispeater where CP_NAME = 'Cheese and Onion'
+except
+select CN_NAME from crispeater where CP_NAME <> 'Cheese and Onion';
+
+/*          ONE BUT NOT ANOTHER        */
+--Consumers who  ate ''Cheese and Onion'' but not ''Buffalo''
+
+select CN_NAME from crispeater where CP_NAME = 'Cheese and Onion'
+except
+select CN_NAME from crispeater where CP_NAME = 'Buffalo';
+
+/*          BOTH        */
+
+--Consumers who  ate ''Cheese and Onion'' and ''Buffalo''
+select CN_NAME from crispeater where CP_NAME = 'Cheese and Onion'
+INTERSECT
+select CN_NAME from crispeater where CP_NAME = 'Buffalo';
+
+/*          EITHER        */
+
+--Consumers who  ate ''Cheese and Onion'' or ''Buffalo''
+select CN_NAME from crispeater where CP_NAME = 'Cheese and Onion'
+UNION
+select CN_NAME from crispeater where CP_NAME = 'Buffalo';
+
+
+/*         EITHER, BUT NOT BOTH (XOR)            */
+
+
+--Consumers who  ate ''Cheese and Onion'' or  ''Buffalo'' but not both.
+select CN_NAME from crispeater where CP_NAME = 'Cheese and Onion'
+UNION
+select CN_NAME from crispeater where CP_NAME = 'Buffalo'
+except
+(select CN_NAME from crispeater where CP_NAME = 'Cheese and Onion'
+INTERSECT
+select CN_NAME from crispeater where CP_NAME = 'Buffalo');
+
+
+
+/*          DIVIDE        */
+/*Return a list of occurrences of A that are related to ALL 
+occurrences of B*/
+
+--Consumers who  ate all types of crisps.
+select CN_NAME from consumer A where not exists
+  (select * from crisp_type B where not exists 
+  (select * from has_eaten X
+    where X.consumerid = A.consumerid
+  and X.crispkey = B.crispkey));
+
